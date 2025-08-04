34af326b51c4f7f87ac3eaa981d225401f10efc0
kerogs <luckywies@gmail.com>
2025-08-04 16:49:14 +0200

    update nav

--- inc/nav.php
+++ inc/nav.php
@@ -31,7 +31,8 @@ $filename = basename($_SERVER['PHP_SELF']);
                     d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
             </svg>
         </a>
-        <a <?= $filename == "caisse.php" ? "class='active'" : "" ?> href="/caisse">
+        <?php if ($_SESSION['account_type'] == "ca") { ?>
+            <a <?= $filename == "caisse.php" ? "class='active'" : "" ?> href="/caisse">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                 class="lucide lucide-layout-grid-icon lucide-layout-grid">
@@ -41,15 +42,18 @@ $filename = basename($_SERVER['PHP_SELF']);
                 <rect width="7" height="7" x="3" y="14" rx="1" />
             </svg>
         </a>
-        <a <?= $filename == "cuisine.php" ? "class='active'" : "" ?> href="/cuisine">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                class="lucide lucide-chef-hat-icon lucide-chef-hat">
-                <path
-                    d="M17 21a1 1 0 0 0 1-1v-5.35c0-.457.316-.844.727-1.041a4 4 0 0 0-2.134-7.589 5 5 0 0 0-9.186 0 4 4 0 0 0-2.134 7.588c.411.198.727.585.727 1.041V20a1 1 0 0 0 1 1Z" />
-                <path d="M6 17h12" />
-            </svg>
-        </a>
+        <?php } ?>
+        <?php if ($_SESSION['account_type'] == "cu") { ?>
+            <a <?= $filename == "cuisine.php" ? "class='active'" : "" ?> href="/cuisine">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-chef-hat-icon lucide-chef-hat">
+                    <path
+                        d="M17 21a1 1 0 0 0 1-1v-5.35c0-.457.316-.844.727-1.041a4 4 0 0 0-2.134-7.589 5 5 0 0 0-9.186 0 4 4 0 0 0-2.134 7.588c.411.198.727.585.727 1.041V20a1 1 0 0 0 1 1Z" />
+                    <path d="M6 17h12" />
+                </svg>
+            </a>
+        <?php } ?>
         <a <?= $filename == "commandes.php" ? "class='active'" : "" ?> href="/commandes">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
@@ -62,7 +66,7 @@ $filename = basename($_SERVER['PHP_SELF']);
                 <path d="M14 20h7" />
             </svg>
         </a>
-        <a <?= $filename == "package.php" ? "class='active'" : "" ?> href="/package">
+        <!-- <a <?= $filename == "package.php" ? "class='active'" : "" ?> href="/package">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                 class="lucide lucide-package-icon lucide-package">
@@ -72,8 +76,8 @@ $filename = basename($_SERVER['PHP_SELF']);
                 <polyline points="3.29 7 12 12 20.71 7" />
                 <path d="m7.5 4.27 9 5.15" />
             </svg>
-        </a>
-        <?php if ($_SESSION['account_type']) { ?>
+        </a> -->
+        <?php if ($_SESSION['account_type'] == "sa") { ?>
             <a <?= $filename == "super_admin.php" ? "class='active'" : "" ?> href="/super_admin">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
